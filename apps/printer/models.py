d85d7a6786af2254057bab303fffe76968ff05f9
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-05 19:22:10 +0300

    Create Printer model

--- apps/printer/models.py
+++ apps/printer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Printer(models.Model):
+    class CheckTypeChoices(models.TextChoices):
+        CLIENT = 'client', 'Client'
+        KITCHEN = 'kitchen', 'Kitchen'
+
+    name = models.CharField(max_length=100)
+    api_key = models.CharField(max_length=100)
+    check_type = models.CharField(max_length=10, choices=CheckTypeChoices.choices)
+    point_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Printers'
+        verbose_name = 'Printer'
+        ordering = ['name']
