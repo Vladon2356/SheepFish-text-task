2a7c96973babe5078327d8e1bac47f3f5b5b5019
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-06 21:42:43 +0300

    Add path param to get pdf for check

--- apps/check/urls.py
+++ apps/check/urls.py
@@ -7,5 +7,5 @@ app_name = 'check'
 urlpatterns = [
     path('checks/', views.CheckApiView.as_view(), name='checks'),
     path('checks/by-printer/', views.GetNonPrintedChecksByPrinter.as_view(), name='checks_by_printer'),
-    path('checks/download-pdf/', views.GetPdfFile.as_view(), name='checks_download_pdf'),
+    path('checks/download-pdf/<str:order_number>/', views.GetPdfFile.as_view(), name='checks_download_pdf'),
 ]
