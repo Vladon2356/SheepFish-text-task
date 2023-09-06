f875b7c57688eb871c92375dc5748ec43ab62b97
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-06 12:18:07 +0300

    Add api view for Printer model

--- apps/printer/views.py
+++ apps/printer/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from apps.printer.models import Printer
+from apps.printer.serializers import PrinterDetailSerializer
+
+
+class PrinterApiView(APIView):
+    """Check API view."""
+
+    def get(self, request):
+        """Get all checks."""
+        checks = Printer.objects.all()
+        data = PrinterDetailSerializer(checks, many=True).data
+        return Response(data)
+
+    def post(self, request):
+        """Create a check."""
+        serializer = PrinterDetailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"id": serializer.data['id']}, status=status.HTTP_201_CREATED)
