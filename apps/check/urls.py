6046061ceac08784ddcb184134680a526a6a33b7
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-06 21:25:00 +0300

    Add new views to urls.py

--- apps/check/urls.py
+++ apps/check/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from apps.check.views import CheckApiView
+from apps.check import views
 
 app_name = 'check'
 
 urlpatterns = [
-    path('checks/', CheckApiView.as_view(), name='checks'),
+    path('checks/', views.CheckApiView.as_view(), name='checks'),
+    path('checks/by-printer/', views.GetNonPrintedChecksByPrinter.as_view(), name='checks_by_printer'),
+    path('checks/download-pdf/', views.GetPdfFile.as_view(), name='checks_download_pdf'),
 ]
