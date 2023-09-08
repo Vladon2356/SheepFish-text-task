215714c8460b5c285932830a28340c6c483af3a3
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-08 18:12:46 +0300

    Add path for create order view and change url for checks_download_pdf

--- apps/check/urls.py
+++ apps/check/urls.py
@@ -5,7 +5,9 @@ from apps.check import views
 app_name = 'check'
 
 urlpatterns = [
+    path('order/', views.OrderCreateView.as_view(), name='order_create'),
+
     path('checks/', views.CheckApiView.as_view(), name='checks'),
     path('checks/by-printer/', views.GetNonPrintedChecksByPrinter.as_view(), name='checks_by_printer'),
-    path('checks/download-pdf/<str:order_number>/', views.GetPdfFile.as_view(), name='checks_download_pdf'),
+    path('checks/download-pdf/<int:check_id>/', views.GetPdfFile.as_view(), name='checks_download_pdf'),
 ]
