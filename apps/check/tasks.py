eaf2552679956c8190b5ed22e883e1273059f5ea
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-08 18:14:51 +0300

    Create task for Celery

--- apps/check/tasks.py
+++ apps/check/tasks.py
@@ -1,12 +1,13 @@
 from core.celery import app
 
+from apps.check.models import Check
 from apps.check.services import generate_pdf_for_check
 
 
 @app.task
-def generate_pdf_for_check_task(data):
+def generate_pdf_for_check_task(check_id):
     try:
-        print('generate_pdf_for_check_task')
-        generate_pdf_for_check(data)
+        check = Check.objects.get(id=check_id)
+        return generate_pdf_for_check(check)
     except Exception as e:
         print(e)
