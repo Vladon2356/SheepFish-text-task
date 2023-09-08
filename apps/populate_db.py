27fbd020916e1fdec63539aff70b4c35d56e4383
Vlad0n20 <vlad.onischuk1234@gmail.com>
2023-09-08 18:20:19 +0300

    Update populate_db scripte

--- apps/populate_db.py
+++ apps/populate_db.py
@@ -11,11 +11,18 @@ point_ids = range(1, 11)
 products = ['milk', 'meet', 'fish', 'apple', 'coca cola', 'pepsi', 'tomato', 'rice', 'potato', 'carrot', 'onion']
 
 
-def create_printer():
+def create_printers_for_point(point_id):
+    Printer.objects.create(
+        name=f'Printer {random.randint(1, 100)}',
+        api_key=f'api_key_{point_id}',
+        point_id=point_id,
+        check_type='client'
+    )
     Printer.objects.create(
         name=f'Printer {random.randint(1, 100)}',
         api_key=f'api_key_{random.randint(1, 100)}',
-        point_id=random.choice(point_ids),
+        point_id=point_id,
+        check_type='kitchen'
     )
 
 
@@ -35,6 +42,6 @@ def create_check(printer_id):
 
 def populate_db(count_printers=11, count_checks=101):
     for i in range(1, count_printers):
-        create_printer()
+        create_printers_for_point(point_id=i)
     for i in range(1, count_checks):
-        create_check(printer_id=random.randint(1, count_printers-1))
+        create_check(printer_id=random.randint(1, 2*count_printers-1))
